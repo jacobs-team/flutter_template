befa9ce3ba35dff11f3bdca5d27d91539d31543c
Jacob <jblankenshipbusiness@gmail.com>
2026-02-01 17:35:29 -0600

    CI

--- lib/app/core/networking/api/endpoints.dart
+++ lib/app/core/networking/api/endpoints.dart
@@ -1,13 +1,13 @@
-/// Centralized route definitions for all services.
+/// Centralized endpoint definitions for all services.
 ///
 /// Use namespaces for service groups:
 /// * [Endpoints.coffee] for coffee services.
 abstract final class Endpoints {
-  /// Routes related to coffee services.
+  /// Endpoints related to coffee services.
   static const coffee = CoffeeRoutes._();
 }
 
-/// Namespace for Coffee routes.
+/// Namespace for Coffee endpoints.
 final class CoffeeRoutes {
   const CoffeeRoutes._();
 
